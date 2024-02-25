e44855eb8cb6a837d623d490c01ffc2fa73d6433
kuxall <kuxall0@gmail.com>
2024-02-25 19:57:58 +0545

    Add main app

---
+++ app.py
@@ -0,0 +1,51 @@
+import streamlit as st
+from translator import translate
+from summarizer import summarize_text
+
+
+def main():
+    st.sidebar.title("Choose an Option")
+    option = st.sidebar.selectbox(
+        "Select the Purpose", ["Translator", "Summarize"])
+
+    if option == "Translator":
+        st.title("Language Translator")
+        dummy_text = st.text_area("Enter the text you want to translate")
+
+        target_language = st.selectbox("Select Target Language", [
+            "Nepali", "French", "Spanish", "German", "Chinese", "Japanese", "Korean", "Russian", "Italian", "Arabic",
+            "Portuguese", "Dutch", "Swedish", "Danish", "Finnish", "Greek", "Hebrew", "Hindi", "Turkish"
+        ])
+
+        if st.button("Translate"):
+            if dummy_text:
+                language_codes = {
+                    "Nepali": "ne", "French": "fr", "Spanish": "es", "German": "de", "Chinese": "zh-CN",
+                    "Japanese": "ja", "Korean": "ko", "Russian": "ru", "Italian": "it", "Arabic": "ar",
+                    "Portuguese": "pt", "Dutch": "nl", "Swedish": "sv", "Danish": "da", "Finnish": "fi",
+                    "Greek": "el", "Hebrew": "iw", "Hindi": "hi", "Turkish": "tr"
+                }
+                trg_lang_code = language_codes.get(target_language)
+
+                translated_text = translate(
+                    src="en", trg=trg_lang_code, text=dummy_text)
+                if translated_text:
+                    st.success("Translated Text:")
+                    st.write(translated_text)
+            else:
+                st.warning("Please enter some text for translation.")
+
+    elif option == "Summarize":
+        st.title("Text Summarizer")
+        text = st.text_area("Please input the text to summarize")
+        if st.button("Summarize"):
+            if text:
+                summary_text = summarize_text(text)
+                st.subheader("Summary:")
+                st.write(summary_text)
+            else:
+                st.warning("Please enter some text to summarize.")
+
+
+if __name__ == "__main__":
+    main()

16a613f964643354c6326138512247405784222e
kuxall <kuxall0@gmail.com>
2024-02-27 09:53:38 +0545

    Modify...

--- summarizer.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from transformers import pipeline
-
-
-@st.cache_data()
-def summarize_text(text):
-    summarizer = pipeline("summarization", model="google-t5/t5-small")
-    summary = summarizer(text)
-    return summary[0]['summary_text']

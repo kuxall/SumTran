43a594e137594a71d8eac42024881c90450ffba9
kuxall <kuxall0@gmail.com>
2024-02-28 10:39:56 +0545

    Add language autodetect functionalities

--- translator/translator.py
+++ translator/translator.py
@@ -1,8 +1,16 @@
-# translator/translator.py
 import requests
 import streamlit as st
+from langdetect import detect
 
+# Language detection function
+def detect_language(text):
+    try:
+        language = detect(text)
+        return language
+    except:
+        return "unknown"
 
+# Translation function
 def translate(src, trg, text):
     url = f"https://translate.googleapis.com/translate_a/single?client=gtx&sl={src}&tl={trg}&dt=t&dt=bd&dj=1&q={text}"
     response = requests.get(url)
